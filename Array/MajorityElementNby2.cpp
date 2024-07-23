301c7c5ae1bcd714714bf83ce17b226477532c1f
SouptikDatta <souptik13@gmail.com>
2024-07-23 23:58:42 +0530

    Day 6 commit

---
+++ Array/MajorityElementNby2.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int cnt = 0; // count
+        int el; // Element
+
+        //applying moores voting algorithm
+        for (int i = 0; i < n; i++) {
+            if (cnt == 0) {
+                cnt = 1;
+                el = nums[i];
+            }
+            else if (el == nums[i]) 
+                cnt++;
+            else cnt--;
+        }
+
+        //checking if the stored element is the majority element
+        int cnt1 = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == el) cnt1++;
+        }
+
+        if (cnt1 > (n / 2)) 
+            return el;
+            
+        return -1;
+    }
+};
---
+++ String/ValidParenthesis.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (char c : s) {
+            // if the character is an opening bracket
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } else { // if the character is a closing bracket
+                if (st.empty() || // if the stack is empty or 
+                    (c == ')' && st.top() != '(') || // the closing bracket don't match the corresponding opening bracket at the top of the stack
+                    (c == '}' && st.top() != '{') ||
+                    (c == ']' && st.top() != '[')) {
+                    return false; // the string is not valid, so return false
+                }
+                st.pop(); // otherwise, pop the opening bracket from the stack
+            }
+        }
+
+        // if the stack is empty, all opening brackets have been matched 
+        //with their corresponding closing brackets
+        return st.empty(); 
+    }
+};
